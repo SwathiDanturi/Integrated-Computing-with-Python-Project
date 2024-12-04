2cbc2de04c5a5d1af835304c773a32d706caedfe
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-12-04 03:59:18 -0500

    implement client.py and clear formatting errors in all files

---
+++ client.py
@@ -0,0 +1,33 @@
+"""
+Test the functionality of the methods implemented in
+analysis_t1.py and analysis_t2.py.
+
+File: client.py
+Date: 12/04/2024
+Developers: Olivia LaCroix, Swathi Danturi
+"""
+
+from analysis_t1 import AnalysisT1
+from analysis_t2 import AnalysisT2
+
+
+def main():
+    """
+    Demo the functionality of the methods implemented in Analysis1 and
+    Analysis2 constructor(s). To run this module, in Terminal,
+    select bash and change directory to PROJECT directory.
+    Then, either use the arrow in upper-right corner, or,
+    in the Terminal, run: python client.py
+    """
+    # Analysis1
+    analysis1 = AnalysisT1("./data/NYPD_Arrest_Data_50_T1.csv")
+    result = analysis1.most_arrests_day_most_common_race()
+    print(result)
+
+    # Analysis2
+    analysis2 = AnalysisT2("./data/NYPD_Arrest_Data_50_T2.csv")
+    result = analysis2.crime_most_committed_agegroup("DRIVING")
+    print(result)
+
+
+main()
--- test_crime_most_committed_agegroup.py
+++ test_crime_most_committed_agegroup.py
@@ -1,5 +1,5 @@
 """
-Test crime_most_committed_agegroup function of analysis_t2.py
+Test crime_most_committed_agegroup method of analysis_t2.py
 
 File: test_crime_most_committed_agegroup.py
 Developer: Swathi Danturi
@@ -7,16 +7,16 @@ Date: 12/01/2024
 """
 
 import pytest
-from analysis_t2 import Analysis
+from analysis_t2 import AnalysisT2
 
 
 def test_crime_most_committed_agegroup_10():
     """
-    Test crime_most_committed_agegroup function of analysis_t2.py
+    Test crime_most_committed_agegroup method of analysis_t2.py
     with NYPD_Arrest_Data_10_T2.csv
     for the crime type 'Intoxicated & Impaired Driving'
     """
-    analysis = Analysis("./data/NYPD_Arrest_Data_10_T2.csv")
+    analysis = AnalysisT2("./data/NYPD_Arrest_Data_10_T2.csv")
     expected = (["25-44", "18-24"], 2, "3:1")
     actual = analysis.crime_most_committed_agegroup("DRIVING")
     assert actual == expected
@@ -24,11 +24,11 @@ def test_crime_most_committed_agegroup_10():
 
 def test_crime_most_committed_agegroup_50():
     """
-    Test crime_most_committed_agegroup function of analysis_t2.py
+    Test crime_most_committed_agegroup method of analysis_t2.py
     with NYPD_Arrest_Data_50_T2.csv
     for the crime type 'Intoxicated & Impaired Driving'
     """
-    analysis = Analysis("./data/NYPD_Arrest_Data_50_T2.csv")
+    analysis = AnalysisT2("./data/NYPD_Arrest_Data_50_T2.csv")
     expected = (["25-44", "<18", "65+"], 3, "9:4")
     actual = analysis.crime_most_committed_agegroup("DRIVING")
     assert actual == expected
@@ -36,11 +36,11 @@ def test_crime_most_committed_agegroup_50():
 
 def test_crime_most_committed_agegroup_100():
     """
-    Test crime_most_committed_agegroup function of analysis_t2.py
+    Test crime_most_committed_agegroup method of analysis_t2.py
     with NYPD_Arrest_Data_100_T2.csv
     for the crime type 'Intoxicated & Impaired Driving'
     """
-    analysis = Analysis("./data/NYPD_Arrest_Data_100_T2.csv")
+    analysis = AnalysisT2("./data/NYPD_Arrest_Data_100_T2.csv")
     expected = (["65+", "25-44"], 7, "22:3")
     actual = analysis.crime_most_committed_agegroup("DRIVING")
     assert actual == expected
