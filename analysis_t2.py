bfce71355c8c9fa1fd6119b195d1288e79129aea
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-12-07 22:41:44 -0500

    implement avg_time_diff_between_arrests() and its utility method date_diff() of analysis_t2.py

--- analysis_t2.py
+++ analysis_t2.py
@@ -37,9 +37,7 @@ class AnalysisT2:
         try:
             with open(file_path, mode="r", encoding="utf-8") as csv_file:
                 csv_reader = csv.DictReader(
-                    csv_file,
-                    delimiter=",",
-                    quotechar='"'
+                    csv_file, delimiter=",", quotechar='"'
                 )
                 self.arrests = list(csv_reader)
         except IOError as err:
@@ -78,3 +76,59 @@ Ratio of crime count between Male and Female is
 '{ratio}'."""
         print(result_string)
         return (highest_count_age_groups, highest_crime_count, ratio)
+
+    def date_diff(self, date1, date2):
+        """
+        calculates the difference in days between two dates
+        called by the method avg_time_diff_between_arrests()
+        """
+        m1, d1, y1 = date1
+        m2, d2, y2 = date2
+        days1 = float(y1 * 365 + m1 * 30 + d1)
+        days2 = float(y2 * 365 + m2 * 30 + d2)
+        return days2 - days1
+
+    def avg_time_diff_between_arrests(self, crime_type):
+        """
+        For each borough, finding the average time difference in days
+        between the arrests for the crime type passed as an argument,
+        according to the age group of the perpetrator
+        """
+        time_diff = {}
+        arrest_days = {}
+        for arrest in self.arrests:
+            if arrest["LAW_CAT_CD"] == crime_type:
+                boro = arrest["ARREST_BORO"]
+                age_group = arrest["AGE_GROUP"]
+                arrest_date = tuple(map(int, arrest["ARREST_DATE"].split("/")))
+                if boro not in arrest_days:
+                    arrest_days[boro] = {}
+                if age_group not in arrest_days[boro]:
+                    arrest_days[boro][age_group] = []
+                arrest_days[boro][age_group].append(arrest_date)
+        for boro, age_groups in arrest_days.items():
+            if boro not in time_diff:
+                time_diff[boro] = {}
+                for age_group, dates in age_groups.items():
+                    dates = sorted(dates)
+                    diffs = [
+                        self.date_diff(dates[i], dates[i + 1])
+                        for i in range(len(dates) - 1)
+                    ]
+                    if diffs:
+                        avg_time_diff = float(sum(diffs) / len(diffs))
+                        time_diff[boro][age_group] = round(avg_time_diff, 2)
+                    else:
+                        time_diff[boro][age_group] = 0
+        result = {}
+        for boro, age_group in time_diff.items():
+            result[boro] = {}
+            for age, time in age_group.items():
+                result[boro][age] = time
+        print(
+            "The Average time difference between arrests for each borough, "
+            "for each age group is of crime type",
+            crime_type,
+            "is:",
+        )
+        return result
